a4e2003117a9ab291043900c6f603b3bc4e18502
HandMK <minkeygod@kangnam.ac.kr>
2025-10-16 23:57:33 +0900

    [Silver V] Title: 수 정렬하기 2, Time: 996 ms, Memory: 120988 KB -BaekjoonHub

--- 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.java
+++ 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.java
@@ -1,31 +1,74 @@
-
 import java.util.*;
 import java.io.*;
 
 public class Main {
 
-    public static BufferedReader br;
-    public static BufferedWriter bw;
+    public static int [] A, tmp;
+    public static long result;
 
     public static void main(String[] args) throws IOException {
-        br = new BufferedReader(new InputStreamReader(System.in));
-        bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        BufferedReader br = new BufferedReader (new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter (new OutputStreamWriter(System.out));
 
         int N = Integer.parseInt(br.readLine());
-        int [] arr = new int[N];
+        A = new int [N+1];
+        tmp = new int [N+1];
 
-        for (int i=0; i<N; i++){
-            int num = Integer.parseInt(br.readLine());
-            arr[i] = num;
+        for (int i=1; i<=N; i++){
+            A[i] = Integer.parseInt(br.readLine());
         }
 
-        Arrays.sort(arr);
+        merge_sort(1, N);
 
-        for (int e : arr){
-            bw.write(e + "\n");
+        for (int i=1; i<=N; i++){
+            bw.write(A[i] + "\n");
         }
 
         bw.flush();
         bw.close();
+    }
+
+    public static void merge_sort(int s, int e){
+        if (e - s < 1){
+            return ;
+        }
+
+        int m = s + (e-s) / 2;
+
+        merge_sort(s, m);
+        merge_sort(m+1, e);
+
+        for (int i=s; i<=e; i++){
+            tmp[i] = A[i];
+        }
+
+        int k = s;
+        int index1 = s;
+        int index2 = m+1;
+
+        while (index1 <= m && index2 <= e){
+
+            if (tmp[index1] > tmp[index2]){
+                A[k] = tmp[index2];
+                k++;
+                index2++;
+            }else{
+                A[k] = tmp[index1];
+                k++;
+                index1++;
+            }
+        }
+
+        while (index1 <= m){ 
+            A[k] = tmp[index1];
+            k++;
+            index1++;
+        }
+
+        while (index2 <= e){
+            A[k] = tmp[index2];
+            k++;
+            index2++;
+        }
     }
 }
