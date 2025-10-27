b437b0ac921759d8397354c2cdddc6b64299eaf6
HandMK <minkeygod@kangnam.ac.kr>
2025-10-27 22:38:21 +0900

    [Silver I] Title: 구간 합 구하기 5, Time: 728 ms, Memory: 119132 KB -BaekjoonHub

--- 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.java
+++ 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.java
@@ -3,39 +3,40 @@ import java.io.*;
 
 public class Main {
 
-    static int N;
-    static int M;
     static StringTokenizer st;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        st = new StringTokenizer(br.readLine());
 
-        st = new StringTokenizer(br.readLine(), " ");
-        N = Integer.parseInt(st.nextToken());
-        M = Integer.parseInt(st.nextToken());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
 
-        int [][] S = new int [N+1][N+1];
+        int[][] map = new int[N+1][N+1];
 
         for (int i=1; i<=N; i++){
-            st = new StringTokenizer(br.readLine(), " ");
+
+            st = new StringTokenizer(br.readLine());
 
             for (int j=1; j<=N; j++){
-                int num = Integer.parseInt(st.nextToken());
-
-                S[i][j] = num + (S[i-1][j] +S[i][j-1]) - S[i-1][j-1];
+                map[i][j] = map[i-1][j] + map[i][j-1] - map[i-1][j-1] + Integer.parseInt(st.nextToken());
             }
         }
 
-        for (int k=0; k<M; k++){
-            st = new StringTokenizer(br.readLine(), " ");
+        for (int i=0; i<M; i++){
+            st = new StringTokenizer(br.readLine());
 
             int x1 = Integer.parseInt(st.nextToken());
             int y1 = Integer.parseInt(st.nextToken());
             int x2 = Integer.parseInt(st.nextToken());
             int y2 = Integer.parseInt(st.nextToken());
 
-            int result = S[x2][y2] - (S[x2][y1-1] + S[x1-1][y2]) + S[x1-1][y1-1];
-            System.out.println(result);
+            int result = map[x2][y2] - (map[x1-1][y2] + map[x2][y1-1]) + map[x1-1][y1-1];
+
+            sb.append(result).append("\n");
         }
+
+        System.out.println(sb);
     }
 }
