74d3312115f3f6260ccda7f1ba0c0cc25e0082f7
HandMK <minkeygod@kangnam.ac.kr>
2025-10-27 22:58:28 +0900

    [Gold III] Title: 나머지 합, Time: 464 ms, Memory: 120640 KB -BaekjoonHub

--- 백준/Gold/10986. 나머지 합/나머지 합.java
+++ 백준/Gold/10986. 나머지 합/나머지 합.java
@@ -1,44 +1,46 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.util.*;
+import java.io.*;
 
 public class Main {
 
+    static StringTokenizer st;
+    static long result;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        st = new StringTokenizer(br.readLine());
 
         int N = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
+        long[] arr = new long[N+1];
 
+        st = new StringTokenizer(br.readLine());
 
-        long[] S = new long[N + 1];
-
-        long[] C = new long[M];
-
-        long answer = 0;
-
-        st = new StringTokenizer(br.readLine(), " ");
-        for (int i = 1; i <= N; i++) {
-            S[i] = S[i - 1] + Integer.parseInt(st.nextToken());
+        for (int i=1; i<=N; i++){
+            arr[i] = arr[i-1] + Integer.parseInt(st.nextToken());
         }
 
-        for (int i = 1; i <= N; i++) {
-            int remainder = (int) (S[i] % M);
+        for (int i=1; i<=N; i++){
+            long tmp = arr[i] % M;
 
-            if (remainder == 0) {
-                answer++;
+            if (tmp == 0){result++;}
+            arr[i] = tmp;
+        }
+
+        long[] check = new long[M];
+
+        for (int i=1; i<=N; i++){
+            int index = (int) arr[i];
+
+            check[index]++;
+        }
+
+        for (long e : check){
+            if (e != 0){
+                result = result + (e * (e-1)) / 2;
             }
-
-            C[remainder]++;
         }
 
-        for (int i = 0; i < M; i++) {
-            if (C[i] > 1) {
-                answer += (C[i] * (C[i] - 1) / 2);
-            }
-        }
-        System.out.println(answer);
+        System.out.println(result);
     }
 }
